7c554852289cc05a3d627195a61d8d967f3b3d71
vanshita-tilwani <msvtilwani@gmail.com>
2023-10-19 14:00:54 -0400

    final changes to a3

--- src/kanbas/courses/coursenavigation/index.js
+++ src/kanbas/courses/coursenavigation/index.js
@@ -2,7 +2,7 @@ import { Link, useParams, useLocation } from "react-router-dom";
 import "./index.css";
 
 function CourseNavigation() {
-  const links = ["Home", "Modules", "Assignments", "Grades"];
+  const links = ["Home", "Modules", "Piazza", "Zoom Meetings", "Assignments", "Quizzes", "Grades", "People"];
   const { courseId } = useParams();
   const { pathname } = useLocation();
   return (
