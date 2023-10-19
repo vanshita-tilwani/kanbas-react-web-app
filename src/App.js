07018e217380cba869c6ab9bea35f51f86227883
vanshita-tilwani <msvtilwani@gmail.com>
2023-10-18 22:54:16 -0400

    Assignment 3 : Lab 2.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './index.css';
+import Labs from "./labs/index";
+import HelloWorld from "./labs/a3/HelloWorld";
+import Kanbas from "./kanbas";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <HelloWorld/>
+      <Labs/>
+      <Kanbas/>
+    </div>  
   );
 }
-
 export default App;
