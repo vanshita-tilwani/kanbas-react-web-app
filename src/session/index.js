0d53263b48c746a3a523ee6c3f852b4620ae3daf
vanshita-tilwani <msvtilwani@gmail.com>
2023-12-09 11:13:00 -0500

    some minor changes

--- src/session/index.js
+++ src/session/index.js
@@ -1,17 +1,15 @@
 
 
-import Signin from "./users/signin";
 import Signup from "./users/signup";
 import Account from "./users/account";
 import { Routes, Route, Navigate } from "react-router-dom";
 import UserTable from "./users/table";
 import Nav from "./users/nav";
+import Signin from "./users/signin";
 
 function Session() {
   return (
-    <div className="container-fluid pt-3">
-      <div className="row">
-        <div className="col-10">
+      
           <Routes>
             <Route path="/nav" element={<Nav/>}/>
             <Route path="/account" element={<Account />} />
@@ -21,9 +19,7 @@ function Session() {
             <Route path="/signup" element={<Signup />} />
             <Route path="/signin" element={<Signin />} />
           </Routes>
-        </div>
-      </div>
-    </div>
+        
   );
 }
 
