aacab0e2c7e59bdea0634a13890131bbeb1a71d7
vanshita-tilwani <msvtilwani@gmail.com>
2023-12-12 14:33:04 -0500

    fixing styling for quiz detail screen

--- src/kanbas/courses/quiz/quizdetails/index.js
+++ src/kanbas/courses/quiz/quizdetails/index.js
@@ -8,94 +8,90 @@ function QuizDetails() {
   const { courseId } = useParams();
   const {quizId} = useParams()
   return (
-    <div className="assignment">
+    <div className="quiz-details-main">
       <div className="d-flex justify-content-between">
         <h3 style={{font : "18px Lato",alignSelf: "center"}} className=" w-25">Quiz Details</h3>
-        <div className="assignment-buttons">
-          
-          <button className="btn btn-danger assignment-button">
+        <div className="quiz-details-buttons">
+          <button className="btn btn-danger quiz-details-button">
             <FaCheckCircle className="icon-margin" />
             Published
           </button>
-          <button className="btn btn-light courses-home-module-button float-end">
-            
+          <button className="btn btn-light quiz-details-button float-end">
             Preview
           </button>
-          
-          <Link to={`/Kanbas/Courses/${courseId}/Quizzes/Edit/${quizId}`} className="btn btn-light courses-home-module-button float-end">
+          <Link to={`/Kanbas/Courses/${courseId}/Quizzes/Edit/${quizId}`} className="btn btn-light quiz-details-button float-end">
             <CiEdit/>
-                  Edit
-                </Link>
+            Edit
+          </Link>
         </div>
       </div>
-      
       <hr />
       <form>
           <div className="container">
             <div className="row p-2">
-              <label className="col-3 assignment-edit-content-title align-self-center">Quiz Type</label>
-              <input disabled type="text" value="Graded Quiz" className="col-3 form-control assignment-edit-content-form"  />
+              <label className="col-3 quiz-detail-content-title align-self-center">Quiz Type</label>
+              <input disabled type="text" value="Graded Quiz" className="col-3 form-control quiz-detail-content-form"  />
             </div>
 
             <div className="row p-2">
-              <label className="col-3 assignment-edit-content-title align-self-center">Points</label>
-              <input disabled type="number" value={29} className="col-3 form-control assignment-edit-content-form"  />
+              <label className="col-3 quiz-detail-content-title align-self-center">Points</label>
+              <input disabled type="number" value={29} className="col-3 form-control quiz-detail-content-form"  />
             </div>
 
             <div className="row p-2">
-              <label className="col-3 assignment-edit-content-title align-self-center">Assignment Group</label>
-              <input disabled type="Text" value="Quizzes" className="col-3 form-control assignment-edit-content-form"  />
+              <label className="col-3 quiz-detail-content-title align-self-center">Assignment Group</label>
+              <input disabled type="Text" value="Quizzes" className="col-3 form-control quiz-detail-content-form"  />
             </div>
 
             <div className="row p-2">
-              <label className="col-3 assignment-edit-content-title align-self-center">Shuffle Answers</label>
-              <input disabled type="text" value="No" className="col-3 form-control assignment-edit-content-form"  />
+              <label className="col-3 quiz-detail-content-title align-self-center">Shuffle Answers</label>
+              <input disabled type="text" value="No" className="col-3 form-control quiz-detail-content-form"  />
             </div>
 
             <div className="row p-2">
-              <label className="col-3 assignment-edit-content-title align-self-center">Time Limit</label>
-              <input disabled type="text" value="30 Minutes" className="col-3 form-control assignment-edit-content-form"  />
+              <label className="col-3 quiz-detail-content-title align-self-center">Time Limit</label>
+              <input disabled type="text" value="30 Minutes" className="col-3 form-control quiz-detail-content-form"  />
             </div>
 
             <div className="row p-2">
-              <label className="col-3 assignment-edit-content-title align-self-center">Multiple Attempts</label>
-              <input disabled type="text" value="No" className="col-3 form-control assignment-edit-content-form"  />
+              <label className="col-3 quiz-detail-content-title align-self-center">Multiple Attempts</label>
+              <input disabled type="text" value="No" className="col-3 form-control quiz-detail-content-form"  />
             </div>
 
             
             <div className="row p-2">
-              <label className="col-3 assignment-edit-content-title align-self-center">View Responses</label>
-              <input disabled type="text" value="Always" className="col-3 form-control assignment-edit-content-form"  />
+              <label className="col-3 quiz-detail-content-title align-self-center">View Responses</label>
+              <input disabled type="text" value="Always" className="col-3 form-control quiz-detail-content-form"  />
             </div>
 
             <div className="row p-2">
-              <label className="col-3 assignment-edit-content-title align-self-center">Show Correct Answers</label>
-              <input disabled type="text" value="Immediately" className="col-3 form-control assignment-edit-content-form"  />
+              <label className="col-3 quiz-detail-content-title align-self-center">Show Correct Answers</label>
+              <input disabled type="text" value="Immediately" className="col-3 form-control quiz-detail-content-form"  />
             </div>
 
             <div className="row p-2">
-              <label className="col-3 assignment-edit-content-title align-self-center">One Question at a Time</label>
-              <input disabled type="Text" value="Yes" className="col-3 form-control assignment-edit-content-form"  />
+              <label className="col-3 quiz-detail-content-title align-self-center">One Question at a Time</label>
+              <input disabled type="Text" value="Yes" className="col-3 form-control quiz-detail-content-form"  />
             </div>
 
             <div className="row p-2">
-              <label className="col-3 assignment-edit-content-title align-self-center">Requires Lockdown Browser</label>
-              <input disabled type="text" value="No" className="col-3 form-control assignment-edit-content-form"  />
+              <label className="col-3 quiz-detail-content-title align-self-center">Requires Lockdown Browser</label>
+              <input disabled type="text" value="No" className="col-3 form-control quiz-detail-content-form"  />
             </div>
 
             <div className="row p-2">
-              <label className="col-3 assignment-edit-content-title align-self-center">Required to View Results</label>
-              <input disabled type="text" value="No" className="col-3 form-control assignment-edit-content-form"  />
+              <label className="col-3 quiz-detail-content-title align-self-center">Required to View Results</label>
+              <input disabled type="text" value="No" className="col-3 form-control quiz-detail-content-form"  />
             </div>
 
             <div className="row p-2">
-              <label className="col-3 assignment-edit-content-title align-self-center">Webcam Required</label>
-              <input disabled type="text" value="No" className="col-3 form-control assignment-edit-content-form"  />
+              <label className="col-3 quiz-detail-content-title align-self-center">Webcam Required</label>
+              <input disabled type="text" value="No" className="col-3 form-control quiz-detail-content-form"  />
             </div>
 
             <div className="row p-2">
-              <label className="col-3 assignment-edit-content-title align-self-center">Lock Questions after Answering</label>
-              <input disabled type="text" value="No" className="col-3 form-control assignment-edit-content-form"  />
+              <label className="col-3 quiz-detail-content-title align-self-center">Lock Questions after Answering</label>
+              <input disabled type="text" value="No" className="col-3 form-control quiz-detail-content-form"  />
             </div>
 
           </div>
